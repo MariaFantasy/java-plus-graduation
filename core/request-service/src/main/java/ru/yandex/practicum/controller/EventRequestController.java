0dbc880c43789d76e4a75b0605cc896ad3eb4e15
Maria <peggy-sju@ya.ru>
2025-08-24 17:05:12 +0300

    Добавляем request-service

--- core/request-service/src/main/java/ru/yandex/practicum/controller/EventRequestController.java
+++ core/request-service/src/main/java/ru/yandex/practicum/controller/EventRequestController.java
@@ -1,4 +1,25 @@
 package ru.yandex.practicum.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.dto.ParticipationRequestDto;
+import ru.yandex.practicum.service.EventRequestService;
+
+@Slf4j
+@RestController
+@RequestMapping("/users/{userId}")
+@RequiredArgsConstructor
 public class EventRequestController {
+    private final EventRequestService eventRequestService;
+
+    @PostMapping("/requests")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ParticipationRequestDto create(@PathVariable Long userId, @RequestParam Long eventId) {
+        log.info("Пришел POST запрос /users/{}/requests?eventId={}", userId, eventId);
+        final ParticipationRequestDto request = eventRequestService.create(userId, eventId);
+        log.info("Отправлен ответ POST /users/{}/requests?eventId={} с телом: {}", userId, eventId, request);
+        return request;
+    }
 }
