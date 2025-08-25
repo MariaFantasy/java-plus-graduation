9d8370aa4729734a00f3ce73ed69958e16328731
Maria <peggy-sju@ya.ru>
2025-08-25 11:22:38 +0300

    Исправление ошибок 14

--- core/request-service/src/main/java/ru/yandex/practicum/controller/EventRequestController.java
+++ core/request-service/src/main/java/ru/yandex/practicum/controller/EventRequestController.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.dto.EventRequestStatusUpdateRequest;
+import ru.yandex.practicum.dto.EventRequestStatusUpdateResult;
 import ru.yandex.practicum.dto.ParticipationRequestDto;
 import ru.yandex.practicum.service.EventRequestService;
+
+import java.util.Collection;
 
 @Slf4j
 @RestController
@@ -21,5 +25,37 @@ public class EventRequestController {
         final ParticipationRequestDto request = eventRequestService.create(userId, eventId);
         log.info("Отправлен ответ POST /users/{}/requests?eventId={} с телом: {}", userId, eventId, request);
         return request;
+    }
+
+    @GetMapping("/requests")
+    public Collection<ParticipationRequestDto> getByRequesterId(@PathVariable Long userId) {
+        log.info("Пришел GET запрос /users/{}/requests", userId);
+        final Collection<ParticipationRequestDto> requests = eventRequestService.getByRequesterId(userId);
+        log.info("Отправлен ответ GET /users/{}/requests с телом: {}", userId, requests);
+        return requests;
+    }
+
+    @PatchMapping("/requests/{requestId}/cancel")
+    public ParticipationRequestDto cancel(@PathVariable Long userId, @PathVariable Long requestId) {
+        log.info("Пришел PATCH запрос /users/{}/requests/{}/cancel", userId, requestId);
+        final ParticipationRequestDto request = eventRequestService.cancel(userId, requestId);
+        log.info("Отправлен ответ PATCH /users/{}/requests/{}/cancel с телом: {}", userId, requestId, request);
+        return request;
+    }
+
+    @GetMapping("/events/{eventId}/requests")
+    public Collection<ParticipationRequestDto> getByEventId(@PathVariable Long userId, @PathVariable Long eventId) {
+        log.info("Пришел GET запрос /users/{}/events/{}/requests", userId, eventId);
+        final Collection<ParticipationRequestDto> requests = eventRequestService.getByEventId(userId, eventId);
+        log.info("Отправлен ответ GET /users/{}/events/{}/requests с телом: {}", userId, eventId, requests);
+        return requests;
+    }
+
+    @PatchMapping("/events/{eventId}/requests")
+    public EventRequestStatusUpdateResult updateStatus(@PathVariable Long userId, @PathVariable Long eventId, @RequestBody EventRequestStatusUpdateRequest requestsToUpdate) {
+        log.info("Пришел PATCH запрос /users/{}/events/{}/requests с телом {}", userId, eventId, requestsToUpdate);
+        final EventRequestStatusUpdateResult result = eventRequestService.updateStatus(userId, eventId, requestsToUpdate);
+        log.info("Отправлен ответ PATCH /users/{}/events/{}/requests с телом: {}", userId, eventId, result);
+        return result;
     }
 }
