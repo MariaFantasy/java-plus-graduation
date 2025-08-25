65e31fc513320649ff70a6f76fae7da8aaad4c20
Maria <peggy-sju@ya.ru>
2025-08-25 11:39:46 +0300

    Исправление ошибок 15

--- core/interaction-api/src/main/java/ru/yandex/practicum/feign/client/EventClient.java
+++ core/interaction-api/src/main/java/ru/yandex/practicum/feign/client/EventClient.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.feign.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.dto.EventFullDto;
 
 @FeignClient(name="event-service", path="/admin/events")
@@ -13,7 +10,7 @@ public interface EventClient {
     @GetMapping("/{eventId}")
     EventFullDto getById(@PathVariable Long eventId);
 
-    @PatchMapping("/confirmed")
+    @PostMapping("/confirmed")
     void updateEventConfirmedRequests(@RequestParam Long eventId, @RequestParam Long confirmedRequests);
 
 }
